c0b6f9a4b3bbe2116d1ea9afafa947ec99085a2c
Dean Gardiner <gardiner91@gmail.com>
2015-08-09 21:17:32 +1200

    Updated [/dist]

--- dist/ladda.js
+++ dist/ladda.js
@@ -1,135 +1,75 @@
-(function(root, factory) {
-    if (typeof exports === "object") {
-        module.exports = factory();
-    } else if (typeof define === "function" && define.amd) {
-        define([ "spin" ], factory);
-    } else {
-        root.Ladda = factory(root.Spinner);
-    }
-})(this, function(Spinner) {
+!function(root, factory) {
+    "object" == typeof exports ? module.exports = factory() : "function" == typeof define && define.amd ? define([ "spin" ], factory) : root.Ladda = factory(root.Spinner);
+}(this, function(Spinner) {
     "use strict";
-    var ALL_INSTANCES = [];
     function create(button) {
-        if (typeof button === "undefined") {
-            console.warn("Ladda button target must be defined.");
-            return;
-        }
-        if (!button.querySelector(".ladda-label")) {
-            button.innerHTML = '<span class="ladda-label">' + button.innerHTML + "</span>";
-        }
-        var spinner = createSpinner(button);
-        var spinnerWrapper = document.createElement("span");
-        spinnerWrapper.className = "ladda-spinner";
-        button.appendChild(spinnerWrapper);
-        var timer;
-        var instance = {
+        if ("undefined" == typeof button) return void console.warn("Ladda button target must be defined.");
+        button.querySelector(".ladda-label") || (button.innerHTML = '<span class="ladda-label">' + button.innerHTML + "</span>");
+        var spinner = createSpinner(button), spinnerWrapper = document.createElement("span");
+        spinnerWrapper.className = "ladda-spinner", button.appendChild(spinnerWrapper);
+        var timer, instance = {
             start: function() {
-                button.setAttribute("disabled", "");
-                button.setAttribute("data-loading", "");
-                clearTimeout(timer);
-                spinner.spin(spinnerWrapper);
-                this.setProgress(0);
-                return this;
+                return button.setAttribute("disabled", ""), button.setAttribute("data-loading", ""), 
+                clearTimeout(timer), spinner.spin(spinnerWrapper), this.setProgress(0), this;
             },
             startAfter: function(delay) {
-                clearTimeout(timer);
-                timer = setTimeout(function() {
+                return clearTimeout(timer), timer = setTimeout(function() {
                     instance.start();
-                }, delay);
-                return this;
+                }, delay), this;
             },
             stop: function() {
-                button.removeAttribute("disabled");
-                button.removeAttribute("data-loading");
-                clearTimeout(timer);
-                timer = setTimeout(function() {
+                return button.removeAttribute("disabled"), button.removeAttribute("data-loading"), 
+                clearTimeout(timer), timer = setTimeout(function() {
                     spinner.stop();
-                }, 1e3);
-                return this;
+                }, 1e3), this;
             },
             toggle: function() {
-                if (this.isLoading()) {
-                    this.stop();
-                } else {
-                    this.start();
-                }
-                return this;
+                return this.isLoading() ? this.stop() : this.start(), this;
             },
             setProgress: function(progress) {
                 progress = Math.max(Math.min(progress, 1), 0);
                 var progressElement = button.querySelector(".ladda-progress");
-                if (progress === 0 && progressElement && progressElement.parentNode) {
-                    progressElement.parentNode.removeChild(progressElement);
-                } else {
-                    if (!progressElement) {
-                        progressElement = document.createElement("div");
-                        progressElement.className = "ladda-progress";
-                        button.appendChild(progressElement);
-                    }
-                    progressElement.style.width = (progress || 0) * button.offsetWidth + "px";
-                }
+                0 === progress && progressElement && progressElement.parentNode ? progressElement.parentNode.removeChild(progressElement) : (progressElement || (progressElement = document.createElement("div"), 
+                progressElement.className = "ladda-progress", button.appendChild(progressElement)), 
+                progressElement.style.width = (progress || 0) * button.offsetWidth + "px");
             },
             enable: function() {
-                this.stop();
-                return this;
+                return this.stop(), this;
             },
             disable: function() {
-                this.stop();
-                button.setAttribute("disabled", "");
-                return this;
+                return this.stop(), button.setAttribute("disabled", ""), this;
             },
             isLoading: function() {
                 return button.hasAttribute("data-loading");
             }
         };
-        ALL_INSTANCES.push(instance);
-        return instance;
+        return ALL_INSTANCES.push(instance), instance;
     }
     function bind(target, options) {
         options = options || {};
         var targets = [];
-        if (typeof target === "string") {
-            targets = toArray(document.querySelectorAll(target));
-        } else if (typeof target === "object" && typeof target.nodeName === "string") {
-            targets = [ target ];
-        }
-        for (var i = 0, len = targets.length; i < len; i++) {
-            (function() {
-                var element = targets[i];
-                if (typeof element.addEventListener === "function") {
-                    var instance = create(element);
-                    var timeout = -1;
-                    element.addEventListener("click", function() {
-                        instance.startAfter(1);
-                        if (typeof options.timeout === "number") {
-                            clearTimeout(timeout);
-                            timeout = setTimeout(instance.stop, options.timeout);
-                        }
-                        if (typeof options.callback === "function") {
-                            options.callback.apply(null, [ instance ]);
-                        }
-                    }, false);
-                }
-            })();
-        }
+        "string" == typeof target ? targets = toArray(document.querySelectorAll(target)) : "object" == typeof target && "string" == typeof target.nodeName && (targets = [ target ]);
+        for (var i = 0, len = targets.length; len > i; i++) !function() {
+            var element = targets[i];
+            if ("function" == typeof element.addEventListener) {
+                var instance = create(element), timeout = -1;
+                element.addEventListener("click", function() {
+                    instance.startAfter(1), "number" == typeof options.timeout && (clearTimeout(timeout), 
+                    timeout = setTimeout(instance.stop, options.timeout)), "function" == typeof options.callback && options.callback.apply(null, [ instance ]);
+                }, !1);
+            }
+        }();
     }
     function stopAll() {
-        for (var i = 0, len = ALL_INSTANCES.length; i < len; i++) {
-            ALL_INSTANCES[i].stop();
-        }
+        for (var i = 0, len = ALL_INSTANCES.length; len > i; i++) ALL_INSTANCES[i].stop();
     }
     function createSpinner(button) {
-        var height = button.offsetHeight, spinnerColor;
-        if (height > 32) {
-            height *= .8;
-        }
-        if (button.hasAttribute("data-spinner-size")) {
-            height = parseInt(button.getAttribute("data-spinner-size"), 10);
-        }
-        if (button.hasAttribute("data-spinner-color")) {
-            spinnerColor = button.getAttribute("data-spinner-color");
-        }
-        var lines = 12, radius = height * .2, length = radius * .6, width = radius < 7 ? 2 : 3;
+        var spinnerColor, height = button.offsetHeight, spinnerTop = "auto", spinnerLeft = "auto";
+        height > 32 && (height *= .8), button.hasAttribute("data-spinner-size") && (height = parseInt(button.getAttribute("data-spinner-size"), 10)), 
+        button.hasAttribute("data-spinner-color") && (spinnerColor = button.getAttribute("data-spinner-color")), 
+        button.hasAttribute("data-spinner-top") && (spinnerTop = button.getAttribute("data-spinner-top")), 
+        button.hasAttribute("data-spinner-left") && (spinnerLeft = button.getAttribute("data-spinner-left"));
+        var lines = 12, radius = .2 * height, length = .6 * radius, width = 7 > radius ? 2 : 3;
         return new Spinner({
             color: spinnerColor || "#fff",
             lines: lines,
@@ -137,18 +77,16 @@
             length: length,
             width: width,
             zIndex: "auto",
-            top: "auto",
-            left: "auto",
+            top: spinnerTop,
+            left: spinnerLeft,
             className: ""
         });
     }
     function toArray(nodes) {
-        var a = [];
-        for (var i = 0; i < nodes.length; i++) {
-            a.push(nodes[i]);
-        }
+        for (var a = [], i = 0; i < nodes.length; i++) a.push(nodes[i]);
         return a;
     }
+    var ALL_INSTANCES = [];
     return {
         bind: bind,
         create: create,
--- dist/spin.js
+++ dist/spin.js
@@ -1,40 +1,26 @@
-(function(root, factory) {
-    if (typeof exports == "object") module.exports = factory(); else if (typeof define == "function" && define.amd) define(factory); else root.Spinner = factory();
-})(this, function() {
+!function(root, factory) {
+    "object" == typeof exports ? module.exports = factory() : "function" == typeof define && define.amd ? define(factory) : root.Spinner = factory();
+}(this, function() {
     "use strict";
-    var prefixes = [ "webkit", "Moz", "ms", "O" ], animations = {}, useCssAnimations;
     function createEl(tag, prop) {
-        var el = document.createElement(tag || "div"), n;
+        var n, el = document.createElement(tag || "div");
         for (n in prop) el[n] = prop[n];
         return el;
     }
     function ins(parent) {
-        for (var i = 1, n = arguments.length; i < n; i++) parent.appendChild(arguments[i]);
+        for (var i = 1, n = arguments.length; n > i; i++) parent.appendChild(arguments[i]);
         return parent;
     }
-    var sheet = function() {
-        var el = createEl("style", {
-            type: "text/css"
-        });
-        ins(document.getElementsByTagName("head")[0], el);
-        return el.sheet || el.styleSheet;
-    }();
     function addAnimation(alpha, trail, i, lines) {
-        var name = [ "opacity", trail, ~~(alpha * 100), i, lines ].join("-"), start = .01 + i / lines * 100, z = Math.max(1 - (1 - alpha) / trail * (100 - start), alpha), prefix = useCssAnimations.substring(0, useCssAnimations.indexOf("Animation")).toLowerCase(), pre = prefix && "-" + prefix + "-" || "";
-        if (!animations[name]) {
-            sheet.insertRule("@" + pre + "keyframes " + name + "{" + "0%{opacity:" + z + "}" + start + "%{opacity:" + alpha + "}" + (start + .01) + "%{opacity:1}" + (start + trail) % 100 + "%{opacity:" + alpha + "}" + "100%{opacity:" + z + "}" + "}", sheet.cssRules.length);
-            animations[name] = 1;
-        }
-        return name;
+        var name = [ "opacity", trail, ~~(100 * alpha), i, lines ].join("-"), start = .01 + i / lines * 100, z = Math.max(1 - (1 - alpha) / trail * (100 - start), alpha), prefix = useCssAnimations.substring(0, useCssAnimations.indexOf("Animation")).toLowerCase(), pre = prefix && "-" + prefix + "-" || "";
+        return animations[name] || (sheet.insertRule("@" + pre + "keyframes " + name + "{0%{opacity:" + z + "}" + start + "%{opacity:" + alpha + "}" + (start + .01) + "%{opacity:1}" + (start + trail) % 100 + "%{opacity:" + alpha + "}100%{opacity:" + z + "}}", sheet.cssRules.length), 
+        animations[name] = 1), name;
     }
     function vendor(el, prop) {
-        var s = el.style, pp, i;
-        if (s[prop] !== undefined) return prop;
-        prop = prop.charAt(0).toUpperCase() + prop.slice(1);
-        for (i = 0; i < prefixes.length; i++) {
-            pp = prefixes[i] + prop;
-            if (s[pp] !== undefined) return pp;
-        }
+        var pp, i, s = el.style;
+        if (void 0 !== s[prop]) return prop;
+        for (prop = prop.charAt(0).toUpperCase() + prop.slice(1), i = 0; i < prefixes.length; i++) if (pp = prefixes[i] + prop, 
+        void 0 !== s[pp]) return pp;
     }
     function css(el, prop) {
         for (var n in prop) el.style[vendor(el, n) || n] = prop[n];
@@ -43,125 +29,25 @@
     function merge(obj) {
         for (var i = 1; i < arguments.length; i++) {
             var def = arguments[i];
-            for (var n in def) if (obj[n] === undefined) obj[n] = def[n];
+            for (var n in def) void 0 === obj[n] && (obj[n] = def[n]);
         }
         return obj;
     }
     function pos(el) {
-        var o = {
+        for (var o = {
             x: el.offsetLeft,
             y: el.offsetTop
-        };
-        while (el = el.offsetParent) o.x += el.offsetLeft, o.y += el.offsetTop;
+        }; el = el.offsetParent; ) o.x += el.offsetLeft, o.y += el.offsetTop;
         return o;
     }
-    var defaults = {
-        lines: 12,
-        length: 7,
-        width: 5,
-        radius: 10,
-        rotate: 0,
-        corners: 1,
-        color: "#000",
-        direction: 1,
-        speed: 1,
-        trail: 100,
-        opacity: 1 / 4,
-        fps: 20,
-        zIndex: 2e9,
-        className: "spinner",
-        top: "auto",
-        left: "auto",
-        position: "relative"
-    };
     function Spinner(o) {
-        if (typeof this == "undefined") return new Spinner(o);
-        this.opts = merge(o || {}, Spinner.defaults, defaults);
+        return "undefined" == typeof this ? new Spinner(o) : void (this.opts = merge(o || {}, Spinner.defaults, defaults));
     }
-    Spinner.defaults = {};
-    merge(Spinner.prototype, {
-        spin: function(target) {
-            this.stop();
-            var self = this, o = self.opts, el = self.el = css(createEl(0, {
-                className: o.className
-            }), {
-                position: o.position,
-                width: 0,
-                zIndex: o.zIndex
-            }), mid = o.radius + o.length + o.width, ep, tp;
-            if (target) {
-                target.insertBefore(el, target.firstChild || null);
-                tp = pos(target);
-                ep = pos(el);
-                css(el, {
-                    left: (o.left == "auto" ? tp.x - ep.x + (target.offsetWidth >> 1) : parseInt(o.left, 10) + mid) + "px",
-                    top: (o.top == "auto" ? tp.y - ep.y + (target.offsetHeight >> 1) : parseInt(o.top, 10) + mid) + "px"
-                });
-            }
-            el.setAttribute("role", "progressbar");
-            self.lines(el, self.opts);
-            if (!useCssAnimations) {
-                var i = 0, start = (o.lines - 1) * (1 - o.direction) / 2, alpha, fps = o.fps, f = fps / o.speed, ostep = (1 - o.opacity) / (f * o.trail / 100), astep = f / o.lines;
-                (function anim() {
-                    i++;
-                    for (var j = 0; j < o.lines; j++) {
-                        alpha = Math.max(1 - (i + (o.lines - j) * astep) % f * ostep, o.opacity);
-                        self.opacity(el, j * o.direction + start, alpha, o);
-                    }
-                    self.timeout = self.el && setTimeout(anim, ~~(1e3 / fps));
-                })();
-            }
-            return self;
-        },
-        stop: function() {
-            var el = this.el;
-            if (el) {
-                clearTimeout(this.timeout);
-                if (el.parentNode) el.parentNode.removeChild(el);
-                this.el = undefined;
-            }
-            return this;
-        },
-        lines: function(el, o) {
-            var i = 0, start = (o.lines - 1) * (1 - o.direction) / 2, seg;
-            function fill(color, shadow) {
-                return css(createEl(), {
-                    position: "absolute",
-                    width: o.length + o.width + "px",
-                    height: o.width + "px",
-                    background: color,
-                    boxShadow: shadow,
-                    transformOrigin: "left",
-                    transform: "rotate(" + ~~(360 / o.lines * i + o.rotate) + "deg) translate(" + o.radius + "px" + ",0)",
-                    borderRadius: (o.corners * o.width >> 1) + "px"
-                });
-            }
-            for (;i < o.lines; i++) {
-                seg = css(createEl(), {
-                    position: "absolute",
-                    top: 1 + ~(o.width / 2) + "px",
-                    transform: o.hwaccel ? "translate3d(0,0,0)" : "",
-                    opacity: o.opacity,
-                    animation: useCssAnimations && addAnimation(o.opacity, o.trail, start + i * o.direction, o.lines) + " " + 1 / o.speed + "s linear infinite"
-                });
-                if (o.shadow) ins(seg, css(fill("#000", "0 0 4px " + "#000"), {
-                    top: 2 + "px"
-                }));
-                ins(el, ins(seg, fill(o.color, "0 0 1px rgba(0,0,0,.1)")));
-            }
-            return el;
-        },
-        opacity: function(el, i, val) {
-            if (i < el.childNodes.length) el.childNodes[i].style.opacity = val;
-        }
-    });
     function initVML() {
         function vml(tag, attr) {
             return createEl("<" + tag + ' xmlns="urn:schemas-microsoft.com:vml" class="spin-vml">', attr);
         }
-        sheet.addRule(".spin-vml", "behavior:url(#default#VML)");
-        Spinner.prototype.lines = function(el, o) {
-            var r = o.length + o.width, s = 2 * r;
+        sheet.addRule(".spin-vml", "behavior:url(#default#VML)"), Spinner.prototype.lines = function(el, o) {
             function grp() {
                 return css(vml("group", {
                     coordsize: s + " " + s,
@@ -171,11 +57,6 @@
                     height: s
                 });
             }
-            var margin = -(o.width + o.length) * 2 + "px", g = css(grp(), {
-                position: "absolute",
-                top: margin,
-                left: margin
-            }), i;
             function seg(i, dx, filter) {
                 ins(g, ins(css(grp(), {
                     rotation: 360 / o.lines * i + "deg",
@@ -195,24 +76,105 @@
                     opacity: 0
                 }))));
             }
+            var i, r = o.length + o.width, s = 2 * r, margin = 2 * -(o.width + o.length) + "px", g = css(grp(), {
+                position: "absolute",
+                top: margin,
+                left: margin
+            });
             if (o.shadow) for (i = 1; i <= o.lines; i++) seg(i, -2, "progid:DXImageTransform.Microsoft.Blur(pixelradius=2,makeshadow=1,shadowopacity=.3)");
             for (i = 1; i <= o.lines; i++) seg(i);
             return ins(el, g);
-        };
-        Spinner.prototype.opacity = function(el, i, val, o) {
+        }, Spinner.prototype.opacity = function(el, i, val, o) {
             var c = el.firstChild;
-            o = o.shadow && o.lines || 0;
-            if (c && i + o < c.childNodes.length) {
-                c = c.childNodes[i + o];
-                c = c && c.firstChild;
-                c = c && c.firstChild;
-                if (c) c.opacity = val;
-            }
+            o = o.shadow && o.lines || 0, c && i + o < c.childNodes.length && (c = c.childNodes[i + o], 
+            c = c && c.firstChild, c = c && c.firstChild, c && (c.opacity = val));
         };
     }
+    var useCssAnimations, prefixes = [ "webkit", "Moz", "ms", "O" ], animations = {}, sheet = function() {
+        var el = createEl("style", {
+            type: "text/css"
+        });
+        return ins(document.getElementsByTagName("head")[0], el), el.sheet || el.styleSheet;
+    }(), defaults = {
+        lines: 12,
+        length: 7,
+        width: 5,
+        radius: 10,
+        rotate: 0,
+        corners: 1,
+        color: "#000",
+        direction: 1,
+        speed: 1,
+        trail: 100,
+        opacity: .25,
+        fps: 20,
+        zIndex: 2e9,
+        className: "spinner",
+        top: "auto",
+        left: "auto",
+        position: "relative"
+    };
+    Spinner.defaults = {}, merge(Spinner.prototype, {
+        spin: function(target) {
+            this.stop();
+            var ep, tp, self = this, o = self.opts, el = self.el = css(createEl(0, {
+                className: o.className
+            }), {
+                position: o.position,
+                width: 0,
+                zIndex: o.zIndex
+            }), mid = o.radius + o.length + o.width;
+            if (target && (target.insertBefore(el, target.firstChild || null), tp = pos(target), 
+            ep = pos(el), css(el, {
+                left: ("auto" == o.left ? tp.x - ep.x + (target.offsetWidth >> 1) : parseInt(o.left, 10) + mid) + "px",
+                top: ("auto" == o.top ? tp.y - ep.y + (target.offsetHeight >> 1) : parseInt(o.top, 10) + mid) + "px"
+            })), el.setAttribute("role", "progressbar"), self.lines(el, self.opts), !useCssAnimations) {
+                var alpha, i = 0, start = (o.lines - 1) * (1 - o.direction) / 2, fps = o.fps, f = fps / o.speed, ostep = (1 - o.opacity) / (f * o.trail / 100), astep = f / o.lines;
+                !function anim() {
+                    i++;
+                    for (var j = 0; j < o.lines; j++) alpha = Math.max(1 - (i + (o.lines - j) * astep) % f * ostep, o.opacity), 
+                    self.opacity(el, j * o.direction + start, alpha, o);
+                    self.timeout = self.el && setTimeout(anim, ~~(1e3 / fps));
+                }();
+            }
+            return self;
+        },
+        stop: function() {
+            var el = this.el;
+            return el && (clearTimeout(this.timeout), el.parentNode && el.parentNode.removeChild(el), 
+            this.el = void 0), this;
+        },
+        lines: function(el, o) {
+            function fill(color, shadow) {
+                return css(createEl(), {
+                    position: "absolute",
+                    width: o.length + o.width + "px",
+                    height: o.width + "px",
+                    background: color,
+                    boxShadow: shadow,
+                    transformOrigin: "left",
+                    transform: "rotate(" + ~~(360 / o.lines * i + o.rotate) + "deg) translate(" + o.radius + "px,0)",
+                    borderRadius: (o.corners * o.width >> 1) + "px"
+                });
+            }
+            for (var seg, i = 0, start = (o.lines - 1) * (1 - o.direction) / 2; i < o.lines; i++) seg = css(createEl(), {
+                position: "absolute",
+                top: 1 + ~(o.width / 2) + "px",
+                transform: o.hwaccel ? "translate3d(0,0,0)" : "",
+                opacity: o.opacity,
+                animation: useCssAnimations && addAnimation(o.opacity, o.trail, start + i * o.direction, o.lines) + " " + 1 / o.speed + "s linear infinite"
+            }), o.shadow && ins(seg, css(fill("#000", "0 0 4px #000"), {
+                top: "2px"
+            })), ins(el, ins(seg, fill(o.color, "0 0 1px rgba(0,0,0,.1)")));
+            return el;
+        },
+        opacity: function(el, i, val) {
+            i < el.childNodes.length && (el.childNodes[i].style.opacity = val);
+        }
+    });
     var probe = css(createEl("group"), {
         behavior: "url(#default#VML)"
     });
-    if (!vendor(probe, "transform") && probe.adj) initVML(); else useCssAnimations = vendor(probe, "animation");
-    return Spinner;
+    return !vendor(probe, "transform") && probe.adj ? initVML() : useCssAnimations = vendor(probe, "animation"), 
+    Spinner;
 });
